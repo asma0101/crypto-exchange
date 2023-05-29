8ae937ca5e7ca77eb7dfd65495e788206bf332ff
asma0101 <asma.official602@gmail.com>
2023-05-29 15:08:16 +0500

    footer class update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,58 +6,58 @@ function Footer() {
             <div className="container-fluid">
                 <div className="row mt-5">
                     <div className="col-md-12">
-                        <footer class="bg-dark text-center text-white footer">
-                            <div class="container p-4 pb-0">
-                                <section class="mb-4">
+                        <footer className="bg-dark text-center text-white footer">
+                            <div className="container p-4 pb-0">
+                                <section className="mb-4">
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{ backgroundColor: iconBgColors[0] }}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-facebook"></i
+                                        ><i className="bi bi-facebook"></i
                                     ></a>
 
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{backgroundColor: iconBgColors[1]}}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-twitter"></i
+                                        ><i className="bi bi-twitter"></i
                                     ></a>
 
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{ backgroundColor: iconBgColors[2] }}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-google"></i
+                                        ><i className="bi bi-google"></i
                                     ></a>
 
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{ backgroundColor: iconBgColors[3] }}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-instagram"></i
+                                        ><i className="bi bi-instagram"></i
                                     ></a>
 
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{ backgroundColor: iconBgColors[4] }}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-linkedin"></i
+                                        ><i className="bi bi-linkedin"></i
                                     ></a>
                                     <a
-                                        class="btn text-white btn-floating m-1"
+                                        className="btn text-white btn-floating m-1"
                                         style={{ backgroundColor: iconBgColors[5] }}
                                         href="#!"
                                         role="button"
-                                        ><i class="bi bi-github"></i
+                                        ><i className="bi bi-github"></i
                                     ></a>
                                 </section>
                             </div>
-                            <div class="text-center p-3" style={{ backgroundColor: iconBgColors[6] }} >
+                            <div className="text-center p-3" style={{ backgroundColor: iconBgColors[6] }} >
                                 © 2020 Copyright: Crypto-Exchange
                             </div>
                         </footer>
