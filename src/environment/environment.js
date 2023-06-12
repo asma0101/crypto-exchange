9c3c378615d64c8bdbd5103c0ac95517166a42c8
asma0101 <asma.official602@gmail.com>
2023-06-12 17:04:46 +0500

    url update env

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -1,6 +1,6 @@
 const environment = {
-  // baseUrl: "https://crypto-exchange-bt4o.onrender.com",
-  baseUrl: 'http://localhost:3006'
+  baseUrl: "https://crypto-exchange-bt4o.onrender.com",
+  // baseUrl: 'http://localhost:3006'
 };
 
 export default environment;
